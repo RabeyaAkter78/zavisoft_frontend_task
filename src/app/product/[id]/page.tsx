f685736cd1c6868a85241d4ec59898b13eef35ef
Rabeya Akter <rabeyariya251@gmail.com>
2026-02-22 15:23:20 +0600

    implement the size according to the figma and disable the color which wre unavailabe

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,43 +3,25 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useGetProductByIdQuery } from "@/redux/api/productsApi";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
-
-interface Product {
-  id: number;
-  title: string;
-  slug: string;
-  price: number;
-  description: string;
-  category: {
-    id: number;
-    name: string;
-    slug: string;
-    image: string;
-    creationAt: string;
-    updatedAt: string;
-  };
-  images: string[];
-  creationAt: string;
-  updatedAt: string;
-}
+import {
+  StaticImageData,
+  StaticImport,
+} from "next/dist/shared/lib/get-img-props";
 
 const ProductDetails = () => {
   const params = useParams();
   const productId = params.id as string;
-
+  const colors = ["#1F2937", "#6B8E6E"];
+  const sizes = [38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
+  const unavailableSizes = [39, 40];
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductByIdQuery(parseInt(productId));
 
-  const [selectedColor, setSelectedColor] = useState("#1F2937");
   const [selectedSize, setSelectedSize] = useState<number | null>(38);
-
-  // Default colors and sizes for demo
-  const colors = ["#1F2937", "#6B8E6E", "#DC2626", "#2563EB"];
-  const sizes = [38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
 
   if (isLoading) {
     return (
@@ -88,26 +70,39 @@ const ProductDetails = () => {
   return (
     <div className="bg-background min-h-screen py-8 px-4 md:px-0">
       <div className="container mx-auto flex flex-col md:flex-row gap-5">
-        {/* LEFT SIDE - IMAGE GRID */}
         <div className="w-full md:w-[70%] grid grid-cols-1 md:grid-cols-2 gap-4  ">
-          {product.images?.map((img: string | StaticImport, index: React.Key | null | undefined) => (
-            <div
-              key={index}
-              className=" flex justify-center items-center bg-background  rounded-tl-2xl "
-            >
-              <Image
-                src={img}
-                alt={product.title}
-                width={429}
-                height={510}
-                className="object-cover w-[429px] h-[510px] rounded-tl-2xl"
-                onError={(e) => {
-                  const target = e.target as HTMLImageElement;
-                  target.src = "/placeholder-image.jpg ";
-                }}
-              />
-            </div>
-          )) || (
+          {product.images
+            ?.slice(0, 4)
+            .map(
+              (
+                img: string | undefined | null | StaticImport | StaticImageData,
+                index: number,
+              ) => {
+                const roundedStyles = [
+                  "rounded-tl-[28px]",
+                  "rounded-tr-[28px]",
+                  "rounded-bl-[28px]",
+                  "rounded-br-[28px]",
+                ];
+
+                return (
+                  <div
+                    key={index}
+                    className={`relative bg-[#f5f5f5] overflow-hidden ${roundedStyles[index]}`}
+                  >
+                    {img && (
+                      <Image
+                        src={img}
+                        alt={product.title}
+                        width={600}
+                        height={700}
+                        className={`w-full h-full object-cover ${roundedStyles[index]}`}
+                      />
+                    )}
+                  </div>
+                );
+              },
+            ) || (
             // Fallback if no images
             <div className="col-span-2 bg-white rounded-2xl overflow-hidden flex items-center justify-center p-6">
               <Image
@@ -121,81 +116,80 @@ const ProductDetails = () => {
           )}
         </div>
 
-        {/* RIGHT SIDE - PRODUCT INFO */}
-        <div className="w-full md:w-[30%]  rounded-2xl p-8">
-          {/* Badge */}
-          <span className="bg-blue-600  text-xs px-4 py-1 rounded-full">
+        <div className="w-full md:w-[30%] ">
+          <span className="bg-[#4d6cdc]   px-4 py-3 rounded-xl font-rubik text-white">
             New Release
           </span>
 
-          {/* Title */}
-          <h1 className="text-2xl md:text-3xl font-bold mt-4 leading-tight">
+          <h1 className="text-xl text-[#232321] md:text-3xl font-semibold mt-4 font-rubik/32px">
             {product.title}
           </h1>
 
-          {/* Price */}
-          <p className="text-blue-600 text-xl font-semibold mt-3">
+          <p className="text-[#4d6cdc] text-2xl  font-semibold my-4 font-rubik/24px ">
             ${product.price}.00
           </p>
 
-          {/* Description */}
-          <p className="text-gray-600 text-sm mt-4">{product.description}</p>
+          <div className="mt-4">
+            <h3 className="font-semibold mb-3 font-rubik">COLOR</h3>
 
-          {/* Category */}
-          <p className="text-gray-500 text-sm mt-2">
-            Category: {product.category?.name}
-          </p>
-
-          {/* COLOR */}
-          <div className="mt-6">
-            <h3 className="text-sm font-semibold mb-3">COLOR</h3>
             <div className="flex gap-3">
               {colors.map((color, index) => (
                 <button
                   key={index}
                   onClick={() => setSelectedColor(color)}
-                  className={`w-8 h-8 rounded-full border-2 ${
-                    selectedColor === color ? "border-black" : "border-gray-300"
-                  }`}
-                  style={{ backgroundColor: color }}
-                />
-              ))}
-            </div>
-          </div>
-
-          {/* SIZE */}
-          <div className="mt-6">
-            <div className="flex justify-between items-center">
-              <h3 className="text-sm font-semibold">SIZE</h3>
-              <span className="text-xs underline cursor-pointer">
-                SIZE CHART
-              </span>
-            </div>
-
-            <div className="grid grid-cols-5 gap-3 mt-3">
-              {sizes.map((size) => (
-                <button
-                  key={size}
-                  onClick={() => setSelectedSize(size)}
-                  className={`py-2 rounded-lg text-sm font-medium border transition-all duration-200 ${
-                    selectedSize === size
-                      ? "bg-black text-white border-black"
-                      : "bg-white border-gray-300 hover:border-black"
+                  className={`w-10 h-10 flex items-center justify-center rounded-full transition-all duration-200 ${
+                    selectedColor === color
+                      ? "border-2 border-black"
+                      : "border-2 border-transparent"
                   }`}
                 >
-                  {size}
+                  <span
+                    className="w-6 h-6 rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
                 </button>
               ))}
             </div>
           </div>
 
-          {/* BUTTONS */}
+          <div className="my-8 font-rubik">
+            <div className="flex justify-between items-center">
+              <h3 className=" font-semibold">SIZE</h3>
+              <span className="underline cursor-pointer">SIZE CHART</span>
+            </div>
+
+            <div className="grid grid-cols-8 gap-1 mt-3">
+              {sizes.map((size) => {
+                const isUnavailable = unavailableSizes.includes(size);
+
+                return (
+                  <button
+                    key={size}
+                    disabled={isUnavailable}
+                    onClick={() => !isUnavailable && setSelectedSize(size)}
+                    className={`p-4 rounded-lg font-medium border transition-all duration-200
+          ${
+            isUnavailable
+              ? "bg-gray-400 text-white border-gray-400 cursor-not-allowed"
+              : selectedSize === size
+                ? "bg-black text-white border-black"
+                : "bg-white border-gray-300 hover:border-black"
+          }
+        `}
+                  >
+                    {size}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+
           <div className="mt-8 space-y-4">
             <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition-all">
               ADD TO CART
             </button>
 
-            <button className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition-all">
+            <button className="w-full bg-[#4d6cdc] text-white py-3 rounded-lg font-semibold hover:bg-blue-700 transition-all">
               BUY IT NOW
             </button>
           </div>
